bba2f6d29333986a3c4a00618685b1a063ed4b6f
ElytherDEV <eyitlesmp@gmail.com>
2026-09-05 03:18:53 +0400

    Update MessageManager.java

--- src/main/java/com/elyther/ecrates/manager/MessageManager.java
+++ src/main/java/com/elyther/ecrates/manager/MessageManager.java
@@ -2,50 +2,81 @@ package com.elyther.ecrates.manager;
 
 import com.elyther.ecrates.ECrates;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 
 public class MessageManager {
 
     private final ECrates plugin;
+
     private YamlConfiguration messages;
 
     public MessageManager(ECrates plugin) {
 
         this.plugin = plugin;
 
-        File file = new File(
-                plugin.getDataFolder(),
-                "messages.yml"
-        );
+        File file =
+                new File(
+                        plugin.getDataFolder(),
+                        "messages.yml"
+                );
 
-        messages = YamlConfiguration.loadConfiguration(file);
+        messages =
+                YamlConfiguration
+                        .loadConfiguration(file);
     }
 
-    public String get(Player player, String key) {
+    public String get(
+            Player player,
+            String key
+    ) {
 
-        String language = plugin.getConfig()
-                .getString("language", "en");
+        String language =
+                plugin.getConfig()
+                        .getString(
+                                "language",
+                                "en"
+                        );
 
-        String text = messages.getString(
-                language + "." + key,
-                key
-        );
+        String message =
+                messages.getString(
+                        language + "." + key,
+                        key
+                );
 
-        return ChatColor.translateAlternateColorCodes(
-                '&',
-                text
-        );
+        return color(message);
     }
 
-    public String get(String language, String key) {
+    public String get(
+            Player player,
+            String key,
+            String... replacements
+    ) {
 
-        String text = messages.getString(
-                language + "." + key,
-                key
-        );
+        String message =
+                get(player, key);
+
+        for (int i = 0;
+             i + 1 < replacements.length;
+             i += 2) {
+
+            message =
+                    message.replace(
+                            replacements[i],
+                            replacements[i + 1]
+                    );
+        }
+
+        return message;
+    }
+
+    private String color(String text) {
+
+        if (text == null) {
+            return "";
+        }
 
         return ChatColor.translateAlternateColorCodes(
                 '&',
